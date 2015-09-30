fc2ca82c42c3241dd67bbb3be54f46fdd1577ac3
tangyao <tangyao@taobao.com>
2015-09-30 23:50:01 +0800

    fix: array encode can be null

---
+++ test/array.test.js
@@ -0,0 +1,50 @@
+/**!
+ * hessian.js - test/array.test.js
+ *
+ * Copyright(c) 2014-2015
+ * MIT Licensed
+ *
+ * Authors:
+ *   汤尧 <tangyao@gmail.com> (http://tangyao.me)
+ */
+
+"use strict";
+
+/**
+ * Module dependencies.
+ */
+
+var should = require('should');
+var hessian = require('../');
+
+describe('array.test.js', function () {
+  it('should write null v1', function () {
+    var b = hessian.encode([
+      {
+        $class: '[java.lang.Integer',
+        $: null
+      },
+      {
+        $class: '[java.lang.Integer',
+        $: [1]
+      }
+    ]);
+    var a = hessian.decode(b);
+    a.should.eql([null, [1]]);
+  });
+
+  it('should write null v2', function () {
+    var b = hessian.encode([
+      {
+        $class: '[java.lang.Integer',
+        $: null
+      },
+      {
+        $class: '[java.lang.Integer',
+        $: [1]
+      }
+    ], '2.0');
+    var a = hessian.decode(b, '2.0');
+    a.should.eql([null, [1]]);
+  });
+});
